baa26a719e8555680c774ce98e2bdfeb9a154981
yxcyy1231 <chenyunxuan1231@gmail.com>
2026-09-24 10:44:05 +0800

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -42,16 +42,16 @@ export default function Navbar() {
   課程介紹
 </Link>
 
-          <Link href="/course" className="transition hover:text-[#8B1E2D]">
-            立即報名
-          </Link>
-
           <Link href="/teachers" className="transition hover:text-[#8B1E2D]">
             師資介紹
           </Link>
 
           <Link href="/classroom" className="transition hover:text-[#8B1E2D]">
             教室環境
+          </Link>
+
+          <Link href="/course" className="transition hover:text-[#8B1E2D]">
+            立即報名
           </Link>
 
           <Link href="/contact" className="transition hover:text-[#8B1E2D]">
@@ -92,14 +92,6 @@ export default function Navbar() {
             </Link>
 
             <Link
-              href="/course"
-              className="px-6 py-4 hover:bg-gray-50"
-              onClick={() => setMenuOpen(false)}
-            >
-              立即報名
-            </Link>
-
-            <Link
               href="/teachers"
               className="px-6 py-4 hover:bg-gray-50"
               onClick={() => setMenuOpen(false)}
@@ -113,6 +105,14 @@ export default function Navbar() {
               onClick={() => setMenuOpen(false)}
             >
               教室環境
+            </Link>
+
+            <Link
+              href="/course"
+              className="px-6 py-4 hover:bg-gray-50"
+              onClick={() => setMenuOpen(false)}
+            >
+              立即報名
             </Link>
 
             <Link
